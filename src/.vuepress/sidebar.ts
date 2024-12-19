12d9e7463cfd75cc7416a8d1e4d972c1ff4f0271
leo <leochuan025@163.com>
2024-12-20 00:17:54 +0800

    配置自动读取侧边栏3

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -9,11 +9,14 @@ export default sidebar({
       prefix: "md/dev-log/",
       children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
     },
+  ],
+  "/broker/": [
     {
       text: "中间件",
       icon: "laptop-code",
       prefix: "md/broker/",
+      collapsible: true,
       children: ['kafka.md', 'redis.md']
     },
-  ],
+  ]
 });
