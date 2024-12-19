41655b94f3d2708c78ba0d80f6c8bb48ad59f3c3
leo <leochuan025@163.com>
2024-12-19 23:40:35 +0800

    配置自动读取侧边栏

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -9,5 +9,10 @@ export default sidebar({
       prefix: "md/dev-log/",
       children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
     },
+    {
+      text: "中间件",
+      icon: "laptop-code",
+      prefix: "md/broker/",
+    },
   ],
 });
