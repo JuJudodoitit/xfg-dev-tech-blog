8aa7c0d0b0153b9a002261f8523d25f4b9874c52
leo <leochuan025@163.com>
2024-12-20 03:35:39 +0800

    配置自动读取侧边栏9

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,15 +1,15 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/md/": [
-    "",
-    {
-      text: "中间件",
-      icon: "book",
-      prefix: "broker/",
-      children: ["kafka/", "redis/"],
-    },
-  ],
+  // "/md/": [
+  //   "",
+  //   {
+  //     text: "中间件",
+  //     icon: "book",
+  //     prefix: "broker/",
+  //     children: ["kafka/", "redis/"],
+  //   },
+  // ],
 
   "/md/broker/redis": "structure",
   "/md/broker/kafka": "structure",
